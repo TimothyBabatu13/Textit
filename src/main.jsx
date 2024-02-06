498b8e5e32990321ca512a8200aa06156b7dff2b
Timothy Babatunde Akanbi <knbtimothy@gmail.com>
2024-02-06 13:53:32 +0100

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const MainApp = App.App;
-const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    
       <MainApp />
-    </QueryClientProvider>
+   
   </React.StrictMode>
 )
